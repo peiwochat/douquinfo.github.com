03f3086b47f74e2687a544f714b4601f718916cf
peiwochat <peiwochat@gmail.com>
2018-04-12 16:52:42 +0800

    Create yq.js

---
+++ yq.js
@@ -0,0 +1,102 @@
+document.writeln("<style>");
+document.writeln(".textlinkmk {float:right;width:95%;}");
+document.writeln(".textlinkmk ul a li {width: 80px;list-style: none;");
+document.writeln("background: #ff9900;float: left;text-align: center;padding: 6px 0px;margin: 1px 1px 0 0;-webkit-transition:all 0.2s;-moz-transition:all 0.2s;-ms-transition:all 0.2s;-o-transition:all 0.2s;}");
+document.writeln(".textlinkmk ul a {font-size: 12px;color: #000000;}");
+document.writeln(".textlinkmk ul a li:hover {background: #DD0838;}");
+document.writeln(".tjyj {float: left;width: 5%;height: 96%;background: #DD0838;color: #000000;font-size: 16px;text-align: center;}");
+document.writeln(".tjyj p {margin-top: 3px;}");
+document.writeln(".textlinkmk ul {");
+document.writeln("    margin: 0;");
+document.writeln("    padding: 0;");
+document.writeln("}");
+document.writeln(".wp {");
+document.writeln("    margin: 0 auto;");
+document.writeln("    width: 100%;");
+document.writeln("}");
+document.writeln("</style>");
+document.writeln("");
+document.writeln("<div class=\'wp\' style=\'height:80px;padding-top:0px;content: \" \";display: table;\'>");
+document.writeln("<div class=\'textlinkmk\'>");
+document.writeln("");
+document.writeln("<ul>");
+document.writeln("<a href=\'http://douqu.info/\' target=\'_blank\' title=\'1\'><li>逗趣福利导航</li></a>");
+document.writeln("<a href=\'http://yehuadh.online/\' target=\'_blank\' title=\'1\'><li>野花福利导航</li></a>");
+document.writeln("<a href=\'http://www.laiyipao.shop/\' target=\'_blank\' title=\'1\'><li>来一炮福利导航</li></a>");
+document.writeln("<a href=\'http://www.qqtube.shop/\' target=\'_blank\' title=\'1\'><li>扣逼福利导航</li></a>");
+document.writeln("<a href=\'http://www.hugedh.xyz/\' target=\'_blank\' title=\'1\'><li>虎哥导航</li></a>");
+document.writeln("<a href=\'http://www.huodh123.com/\' target=\'_blank\' title=\'1\'><li>火导航</li></a>");
+document.writeln("<a href=\'http://www.selangdh.com/\' target=\'_blank\' title=\'1\'><li>狼导航</li></a>");
+document.writeln("<a href=\'http://younvdaohang.com/\' target=\'_blank\' title=\'1\'><li>幼女视频导航</li></a>");
+document.writeln("<a href=\'http://www.jpmdh.xyz/\' target=\'_blank\' title=\'1\'><li>金瓶梅导航</li></a>");
+document.writeln("<a href=\'http://fbdh.me/\' target=\'_blank\' title=\'1\'><li>粉逼导航</li></a>");
+document.writeln("<a href=\'http://www.f2dh.top/\' target=\'_blank\' title=\'1\'><li>富二代导航</li></a>");
+document.writeln("<a href=\'http://www.90hdh.info/\' target=\'_blank\' title=\'1\'><li>90后成人导航</li></a>");
+document.writeln("<a href=\'http://www.mglsdh.pw/\' target=\'_blank\' title=\'1\'><li>美国绿色导航</li></a>");
+document.writeln("<a href=\'http://www.dgrdh.top/\' target=\'_blank\' title=\'1\'><li>大官人导航</li></a>");
+document.writeln("<a href=\'http://www.mccdh3.pw/\' target=\'_blank\' title=\'1\'><li>MCC名站导航</li></a>");
+document.writeln("<a href=\'http://www.13mzdh.info/\' target=\'_blank\' title=\'1\'><li>13名站导航</li></a>");
+document.writeln("<a href=\'http://www.2000dh.top\' target=\'_blank\' title=\'1\'><li>2000名站导航</li></a>");
+document.writeln("<a href=\'http://www.gblw.biz/\' target=\'_blank\' title=\'1\'><li>隔壁老王导航</li></a>");
+document.writeln("<a href=\'http://luolidaohang.com/\' target=\'_blank\' title=\'1\'><li>萝莉视频导航</li></a>");
+document.writeln("<a href=\'http://xiaoxuesheng.pw/\' target=\'_blank\' title=\'1\'><li>小学生导航</li></a>");
+document.writeln("<a href=\'http://mogutou.pw/\' target=\'_blank\' title=\'1\'><li>蘑菇头导航</li></a>");
+document.writeln("<a href=\'http://www.djjrdh.com\' target=\'_blank\' title=\'1\'><li>点击进入导航</li></a>");
+document.writeln("<a href=\'http://www.jsavdh.pw\' target=\'_blank\' title=\'1\'><li>聚色导航</li></a>");
+document.writeln("<a href=\'http://www.8090dh.top/\' target=\'_blank\' title=\'1\'><li>8090导航</li></a>");
+document.writeln("<a href=\'http://www.cldh99.pw\' target=\'_blank\' title=\'1\'><li>草榴色导航</li></a>");
+document.writeln("<a href=\'http://www.avs66.pw/\' target=\'_blank\' title=\'1\'><li>AVS成人导航</li></a>");
+document.writeln("<a href=\'http://www.grdh99.pw/\' target=\'_blank\' title=\'1\'><li>国人色导航</li></a>");
+document.writeln("<a href=\'http://www.xbdh8.top/\' target=\'_blank\' title=\'1\'><li>性吧色导航</li></a>");
+
+document.writeln("<a href=\'http://www.73dh.pw/\' target=\'_blank\' title=\'1\'><li>情兽色导航</li></a>");
+document.writeln("<a href=\'http://www.70dh.pw/\' target=\'_blank\' title=\'1\'><li>70色导航</li></a>");
+document.writeln("<a href=\'http://www.71dh.pw/\' target=\'_blank\' title=\'1\'><li>小姐姐色导航</li></a>");
+document.writeln("<a href=\'http://www.72dh.pw/\' target=\'_blank\' title=\'1\'><li>妻爱导航</li></a>");
+document.writeln("<a href=\'http://www.74dh.pw/\' target=\'_blank\' title=\'1\'><li>撸一发导航</li></a>");
+document.writeln("<a href=\'http://www.75dh.pw/\' target=\'_blank\' title=\'1\'><li>情网色导航</li></a>");
+document.writeln("<a href=\'http://www.76dh.pw/\' target=\'_blank\' title=\'1\'><li>蝴蝶逼导航</li></a>");
+document.writeln("<a href=\'http://www.77dh.pw/\' target=\'_blank\' title=\'1\'><li>七七色导航</li></a>");
+document.writeln("<a href=\'http://www.79dh.pw/\' target=\'_blank\' title=\'1\'><li>啪啪小姨子</li></a>");
+document.writeln("<a href=\'http://www.78dh.pw/\' target=\'_blank\' title=\'1\'><li>潮喷秀导航</li></a>");
+document.writeln("<a href=\'http://www.60dh.pw/\' target=\'_blank\' title=\'1\'><li>大骚逼导航</li></a>");
+document.writeln("<a href=\'http://www.61dh.pw/\' target=\'_blank\' title=\'1\'><li>舔阴沟导航</li></a>");
+document.writeln("<a href=\'http://www.62dh.pw/\' target=\'_blank\' title=\'1\'><li>性饥饿导航</li></a>");
+document.writeln("<a href=\'http://www.63dh.pw\' target=\'_blank\' title=\'1\'><li>抽插导航</li></a>");
+document.writeln("<a href=\'http://www.64dh.pw/\' target=\'_blank\' title=\'1\'><li>淫水多导航</li></a>");
+document.writeln("<a href=\'http://www.65dh.pw/\' target=\'_blank\' title=\'1\'><li>大鸡巴导航</li></a>");
+document.writeln("<a href=\'http://www.66dh.pw/\' target=\'_blank\' title=\'1\'><li>驴鸡巴导航</li></a>");
+document.writeln("<a href=\'http://www.67dh.pw\' target=\'_blank\' title=\'1\'><li>狼牙棒导航</li></a>");
+document.writeln("<a href=\'http://www.68dh.pw/\' target=\'_blank\' title=\'1\'><li>干弟弟导航</li></a>");
+document.writeln("<a href=\'http://www.69dh.pw/\' target=\'_blank\' title=\'1\'><li>小色逼导航</li></a>");
+document.writeln("<a href=\'http://www.1dr2.top/\' target=\'_blank\' title=\'1\'><li>爸逼伦色导航</li></a>");
+document.writeln("<a href=\'http://www.456p.top/\' target=\'_blank\' title=\'1\'><li>干幼女色导航</li></a>");
+document.writeln("<a href=\'http://www.56sf.top/\' target=\'_blank\' title=\'1\'><li>操小姐姐导航</li></a>");
+document.writeln("<a href=\'http://www.kh32.top/\' target=\'_blank\' title=\'1\'><li>看逼视频导航</li></a>");
+document.writeln("<a href=\'http://www.ngrr3.top/\' target=\'_blank\' title=\'1\'><li>日仨逼导航</li></a>");
+document.writeln("<a href=\'http://www.sf142.top/\' target=\'_blank\' title=\'1\'><li>就要你干导航</li></a>");
+document.writeln("<a href=\'http://www.315073.top/\' target=\'_blank\' title=\'1\'><li>姐姐好痒导航</li></a>");
+
+
+document.writeln("<a href=\'http://www.500daohang.com/\' target=\'_blank\' title=\'1\'><li>500导航</li></a>");
+document.writeln("<a href=\'http://yasedh.xyz/\' target=\'_blank\' title=\'1\'><li>亚色导航</li></a>");
+document.writeln("<a href=\'http://hongdoudh.xyz/\' target=\'_blank\' title=\'1\'><li>红豆导航</li></a>");
+document.writeln("<a href=\'http://www.soav33.com/\' target=\'_blank\' title=\'1\'><li>伊人导航</li></a>");
+document.writeln("<a href=\'http://yuepaodh.top/\' target=\'_blank\' title=\'1\'><li>约炮导航</li></a>");
+document.writeln("<a href=\'http://www.16880.cf/\' target=\'_blank\' title=\'1\'><li>日日福利导航</li></a>");
+
+
+
+
+
+
+
+
+
+
+document.writeln("</ul>");
+document.writeln("</div>");
+document.writeln("<div class=\'tjyj\'>");
+document.writeln("<p>友<br/>情<br/>链<br/>接</p>");
+document.writeln("</div>");
+document.writeln("</div>");
